9e2576781b1e49a10862ce41631e5bac6398738d
Trolloldem <oldanigianluca@gmail.com>
2019-06-24 18:55:41 +0200

    Add files via upload

--- Implementation.py
+++ Implementation.py
@@ -3,15 +3,19 @@ from sys import argv
 from sys import exit
 from MazeGenerator import mazeGen
 from NodeClass import Node
-from datetime import datetime
+import datetime
+from _datetime import datetime, timedelta
 from PIL import Image
 import argparse
 
 
 class A_StarResult:
-    def __init__(self,path,image):
+    def __init__(self,path,image,visitedNodes,timeUsed,pathCost):
         self.path=path
         self.image=image
+        self.visitedNodes=visitedNodes
+        self.timeUsed=timeUsed
+        self.pathCost=pathCost
 
 def euclideanDistance(point1,point2):
     return math.sqrt(math.pow(point1[0] - point2[0], 2) + math.pow(point1[1] - point2[1], 2))
@@ -28,7 +32,7 @@ def getPath(destination,image):
         image.putpixel(destination.getXY(),(255,0,0))
         path.append(destination.getXY())
         destination=destination.getParent()
-    print("Numero nodi nel cammino:"+str(count-1))
+    print("Numero nodi nel cammino:"+str(count))
     path.reverse()
     return path
 
@@ -51,13 +55,13 @@ def A_StarPathfind(image,start,endCoord,dim,hFun):
         neighbors = []
         actual=openNodes[0]
         if(actual.getXY()==endCoord):
-            result= A_StarResult(getPath(actual,image),image)
             endTime = datetime.now()
             print("Tempo utilizzato:")
             print(endTime-startTime)
             print("Nodi in frontiera: " + str(len(openNodes)))
-            print("Nodi esplorati: " + str(len(closedNodes)))
+            print("Nodi esplorati: " + str(len(closedNodes)+1))
             print("Costo del cammino: " + str(actual.getGValue()))
+            result = A_StarResult(getPath(actual, image), image,len(closedNodes)+1,endTime-startTime,actual.getGValue())
             return result
 
         actualCoord = actual.getXY()
@@ -89,6 +93,7 @@ def A_StarPathfind(image,start,endCoord,dim,hFun):
                 neighbors.append(Node(actualCoord[0] + 1, actualCoord[1]-1, actual, hFun((actualCoord[0] + 1, actualCoord[1]-1), endCoord),actual.getGValue() + euclideanDistance(actualCoord,(actualCoord[0] + 1, actualCoord[1]-1))))
 
         #FINE DIREZIONI DI TEST
+
         for neighbor in neighbors:
             if (image.getpixel(neighbor.getXY()) == (0, 0, 255)): #il nodo è già in frontiera => controllo il suo costo attuale
                 for node in openNodes:
@@ -108,48 +113,94 @@ def A_StarPathfind(image,start,endCoord,dim,hFun):
 
 
 
-def main(dim,type,show=False):
-    if(type=="random"):
-        specificName="random"
-        image=mazeGen(dim) #generazione labirinto
-    if(type=="critica"):
-        specificName = "critica"
-        image=Image.open("critica.png")
-        dim=250
-    if (type == "empty"):
-        specificName = "empty"
-        image = Image.open("empty.png")
-        dim=200
-    if (type == "example"):
-        specificName = "example"
-        image = Image.open("creata.png")
-        dim=200
-    if (type == "bench"):
-        specificName = "example"
-        image = Image.open("bench.png")
-        dim=820
-        startCoord = (210, 10)
-        endCoord = (800,805)
+def main(dim,type,run,show=False):
+
+    euclideanTimes=[]
+    euclideanNodes=[]
+    euclideanLenPath=[]
+    ManhattanTimes = []
+    ManhattanNodes = []
+    ManhattanLenPath = []
+    DijTimes = []
+    DijNodes = []
+    DijLenPath = []
+
+    for i in range(0,run):
+        if(type=="random"):
+            specificName="random"
+            image=mazeGen(dim) #generazione labirinto
+        if(type=="critica"):
+            specificName = "critica"
+            image=Image.open("critica.png")
+            dim=250
+        if (type == "empty"):
+            specificName = "empty"
+            image = Image.open("empty.png")
+            dim=200
+        if (type == "example"):
+            specificName = "example"
+            image = Image.open("creata.png")
+            dim=200
+        if (type == "bench"):
+            specificName = "example"
+            image = Image.open("bench.png")
+            dim=820
+            startCoord = (210, 10)
+            endCoord = (800,805)
 
 
-    imageEuClid=image.copy()
-    imageDij=image.copy()
-    if(type!="bench"):
-        startCoord = (0,0)
-        endCoord = (dim-1,dim-1)
-    endCoord = moveNotWall(endCoord,image,-1)
-    startCoord = moveNotWall(startCoord, image, +1)
-    start = Node(startCoord[0], startCoord[1], None, euclideanDistance((0, 0), (dim - 1, dim - 1)), 0)
-    resultEuclid=A_StarPathfind(imageEuClid,start,endCoord,dim,euclideanDistance)
-    resultMan=A_StarPathfind(image,start,endCoord,dim,ManhattanDistance)
-    resultDij = A_StarPathfind(imageDij, start, endCoord, dim,costantDistanceZero)
-    resultDij.image.save("SolvedMaze_"+specificName+"_"+ str(dim) + "x" + str(dim) + "_Dij.png", "PNG")
-    resultEuclid.image.save("SolvedMaze_" +specificName+"_"+ str(dim) + "x" + str(dim) + "_Euclidean.png", "PNG")
-    resultMan.image.save("SolvedMaze_" + specificName+"_"+str(dim) + "x" + str(dim) + "_Manhattan.png", "PNG")
-    if show:
-        resultDij.image.show()
-        resultMan.image.show()
-        resultEuclid.image.show()
+        imageEuClid=image.copy()
+        imageDij=image.copy()
+
+        if(type!="bench"):
+            startCoord = (0,0)
+            endCoord = (dim-1,dim-1)
+        endCoord = moveNotWall(endCoord,image,-1)
+        startCoord = moveNotWall(startCoord, image, +1)
+        start = Node(startCoord[0], startCoord[1], None, euclideanDistance((0, 0), (dim - 1, dim - 1)), 0)
+
+        resultEuclid=A_StarPathfind(imageEuClid,start,endCoord,dim,euclideanDistance)
+        euclideanTimes.append(resultEuclid.timeUsed)
+        euclideanNodes.append(resultEuclid.visitedNodes)
+        euclideanLenPath.append(resultEuclid.pathCost)
+
+        resultMan=A_StarPathfind(image,start,endCoord,dim,ManhattanDistance)
+        ManhattanTimes.append(resultMan.timeUsed)
+        ManhattanNodes.append(resultMan.visitedNodes)
+        ManhattanLenPath.append(resultMan.pathCost)
+
+        resultDij = A_StarPathfind(imageDij, start, endCoord, dim,costantDistanceZero)
+        DijTimes.append(resultDij.timeUsed)
+        DijNodes.append(resultDij.visitedNodes)
+        DijLenPath.append(resultDij.pathCost)
+
+        resultDij.image.save("SolvedMaze_"+specificName+"_"+ str(dim) + "x" + str(dim) + "_Dij.png", "PNG")
+        resultEuclid.image.save("SolvedMaze_" +specificName+"_"+ str(dim) + "x" + str(dim) + "_Euclidean.png", "PNG")
+        resultMan.image.save("SolvedMaze_" + specificName+"_"+str(dim) + "x" + str(dim) + "_Manhattan.png", "PNG")
+
+        if show:
+            resultDij.image.show()
+            resultMan.image.show()
+            resultEuclid.image.show()
+
+    if run!=1:
+        print("Misurazioni euristica euclidea:")
+        print("Media dei tempi:"+str(sum(euclideanTimes, timedelta(0)) / len(euclideanTimes)))
+        print("Media dei nodi esplorati:"+str(sum(euclideanNodes,0)/len(euclideanNodes)))
+        print("Misurazioni euristica Manhattan:")
+        print("Media dei tempi:" + str(sum(ManhattanTimes, timedelta(0)) / len(ManhattanTimes)))
+        print("Media dei nodi esplorati:" + str(sum(ManhattanNodes, 0) / len(ManhattanNodes)))
+        print("Misurazioni Dijkstra:")
+        print("Media dei tempi:" + str(sum(DijTimes, timedelta(0)) / len(DijTimes)))
+        print("Media dei nodi esplorati:" + str(sum(DijNodes, 0) / len(DijNodes)))
+        alwaysOptimal=True
+        for i in range(0,len(euclideanLenPath)):
+            if not(euclideanLenPath[i]==DijLenPath[i] and ManhattanLenPath[i]==DijLenPath[i]):
+                alwaysOptimal=False
+        if alwaysOptimal:
+            print("Ottimo sempre raggiunto")
+        else:
+            print("Non viene sempre raggiunto l'ottimo")
 
 if __name__ == "__main__":
     argumentList = argv[1:]
@@ -158,6 +209,7 @@ if __name__ == "__main__":
     parser.add_argument('type', metavar='tipo', type=str,
                         help='Tipo di run: "random" per labirinto random, "empty" per immagine bianca, "critica" per immagine problematica, "example" per un\'immagine generica, "bench" per un\'immagine di benchmark')
     parser.add_argument('--show','-s',help='Mostra le immagini a fine computazione',action="store_true")
+    parser.add_argument('--run', '-r', type=int,help='Numero di run consecutive', dest="run")
     args = parser.parse_args()
     if args.type=="random" and args.dim!=None:
         print("Genero labirinto di dimensione:"+str(args.dim))
@@ -166,9 +218,11 @@ if __name__ == "__main__":
         exit()
     if args.dim==None:
         args.dim=0
+    if args.run==None:
+        args.run=1
     if args.show:
         print("L'immagine verrà visualizzata")
-    main(args.dim,args.type,args.show)
+    main(args.dim,args.type,args.run,args.show)
 
 
 
